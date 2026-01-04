03a69bc37009be95be576d6425869946283dc278
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-04 16:10:07 +0200

    feat: add checkout controller and route for payment processing

---
+++ backend/src/controllers/checkoutController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import Stripe from "stripe";
+import Product from "../models/product";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+if (!stripe) {
+  throw new Error(
+    "Stripe initialization failed. Check your STRIPE_SECRET_KEY."
+  );
+}
+export const checkoutController = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.body;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: product.name as string,
+              images: [product.image as string],
+            },
+            unit_amount: Math.round(product.price * 100), // استخدام Math.round لضمان رقم صحيح
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `${process.env.CLIENT_URL}/success`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel`,
+    });
+
+    res.json({ url: session.url });
+  } catch (error: any) {
+    console.error("Stripe Error:", error.message);
+    res.status(500).json({
+      status: "error",
+      message: "Payment session creation failed",
+    });
+  }
+};
---
+++ backend/src/routes/checkoutRoute.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { checkoutController } from "../controllers/checkoutController";
+const router = Router();
+
+router.post("/checkout", checkoutController);
+
+export default router;
