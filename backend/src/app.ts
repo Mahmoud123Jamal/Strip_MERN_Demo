a8332ebf1cb80a8e0aa23b96a607099f3510d91a
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-04 16:10:18 +0200

    feat: import and configure checkout and product routes in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
-
+import checkoutRoute from "./routes/checkoutRoute";
+import product from "./routes/productRoute";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors());
+
+app.use("/api", checkoutRoute);
+app.use("/api", product);
 
 export default app;
