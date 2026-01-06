ca9445692f8caa6921f353b0cefd1f3eb692acbf
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-06 20:42:22 +0200

    feat: add webhook route middleware for handling raw JSON requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import checkoutRoute from "./routes/checkoutRoute";
 import product from "./routes/productRoute";
+import webhookRoute from "./routes/webhookRoute";
 const app = express();
-
+app.use("/api", express.raw({ type: "application/json" }), webhookRoute);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
