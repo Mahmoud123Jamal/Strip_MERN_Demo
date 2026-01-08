5d0365b9116e778b214e0b7539e34fd4a1608129
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-08 19:55:15 +0200

    update createProduct to add stock & lockedStock

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express";
 import Product from "../models/product";
-
 export const createProduct = async (req: Request, res: Response) => {
   try {
-    const { name, image, price } = req.body;
-    const product = await Product.create({ name, image, price });
-    res.status(200).json({ status: "success", data: { product } });
-  } catch (error) {
+    const { name, image, price, stock } = req.body;
+
+    const product = await Product.create({
+      name,
+      image,
+      price,
+      stock: stock ?? 0,
+      lockedStock: 0,
+    });
+
+    res.status(201).json({ status: "success", data: { product } });
+  } catch (error: any) {
+    console.error("Create Product Error:", error.message);
     res.status(500).json({
       status: "error",
-      message: "Error in product controller",
+      message: "Error creating product",
     });
   }
 };
