42b9b6c8b2d0d9debaf282df7e01839d72a6c212
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-08 18:43:49 +0200

    update product model (add stock , lockedStock)

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -1,10 +1,22 @@
 import { model, Schema } from "mongoose";
-import { Iproduct } from "./../types/productType";
+import { Iproduct } from "../types/productType";
 
-const productSchem = new Schema<Iproduct>({
+const productSchema = new Schema<Iproduct>({
   name: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
+
+  stock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  lockedStock: {
+    type: Number,
+    default: 0,
+  },
 });
-const Product = model<Iproduct>("Product", productSchem);
+
+const Product = model<Iproduct>("Product", productSchema);
 export default Product;
