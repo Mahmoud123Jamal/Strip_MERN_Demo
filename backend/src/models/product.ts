9ab9bc37483892f5da05b996cb19288434258088
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-10 00:44:40 +0200

    update productSchema

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -3,20 +3,11 @@ import { Iproduct } from "../types/productType";
 
 const productSchema = new Schema<Iproduct>({
   name: { type: String, required: true },
-  image: { type: String, required: true },
   price: { type: Number, required: true },
-
-  stock: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-
-  lockedStock: {
-    type: Number,
-    default: 0,
-  },
-  lockExpiresAt: { type: Date },
+  image: { type: String, required: true },
+  stock: { type: Number, required: true, min: 0, default: 0 },
+  reserved: { type: Number, default: 0, min: 0 },
+  lockExpiresAt: { type: Date, default: null },
 });
 
 const Product = model<Iproduct>("Product", productSchema);
