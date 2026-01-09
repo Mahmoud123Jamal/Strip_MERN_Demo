0210f69357eab1ba8ce63cfdf63c3366c379a569
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-10 00:46:48 +0200

    import cleanupExpiredLocks and use cron.schedule to cleanup job every 1 minute

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import app from "./app";
 import { connectDB } from "./config/db";
+import cron from "node-cron";
+import { cleanupExpiredLocks } from "./jobs/cleanupExpiredLocks";
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+// Run cleanup job every 1 minute
+cron.schedule("* * * * *", cleanupExpiredLocks);
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
